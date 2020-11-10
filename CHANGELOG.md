646e7763d7b4044a50f3879ca573393f5af9ff58
johnMedlockDev <john.medlock.dev@gmail.com>
2020-11-10 15:15:34 -0600

    Added function to convert binary to decimal. Project Finshed.

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## Version 1.0
+### Added:
+- Function to make conversion from binary to decimal.
--- Program.cs
+++ Program.cs
@@ -6,7 +6,55 @@ namespace Bin2Dec
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach (string arg in args)
+            {
+                Console.WriteLine(Converter(arg));
+            }
+
+            /// <summary>
+            /// Converts a given cli argument from binary to it's decimal value
+            /// </summary>
+            static int Converter(string arg)
+            {
+
+                int sum = 0;
+                int count = 0;
+
+                foreach (char c in arg)
+                {
+                    count++;
+                    int i = Convert.ToInt32(c.ToString());
+                    switch (count)
+                    {
+                        case 1:
+                            sum += i * 128;
+                            break;
+                        case 2:
+                            sum += i * 64;
+                            break;
+                        case 3:
+                            sum += i * 32;
+                            break;
+                        case 4:
+                            sum += i * 16;
+                            break;
+                        case 5:
+                            sum += i * 8;
+                            break;
+                        case 6:
+                            sum += i * 4;
+                            break;
+                        case 7:
+                            sum += i * 2;
+                            break;
+                        case 8:
+                            sum += i * 1;
+                            break;
+                    }
+                }
+                return sum;
+
+            }
         }
     }
 }
